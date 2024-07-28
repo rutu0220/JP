f045d4b91de2dc984f447a2d4fe0d9d05f90fe39
Rutuja Athane <109471615+rutu0220@users.noreply.github.com>
2024-07-28 19:41:37 +0530

    Add files via upload

---
+++ addInt.java
@@ -0,0 +1,13 @@
+import java.util.*;
+import java.lang.*;
+
+class addint
+{
+ public static void main(String args[])
+{
+  float num1=Float.parseFloat(args[0]);
+  float num2=Float.parseFloat(args[1]);
+  float sum=num1+num2;
+  System.out.println("Sum" + sum);
+}
+}
